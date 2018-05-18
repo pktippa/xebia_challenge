d401237ac3de4f6e7a282bc205c37170405f72ad
Pradeep Kumar Tippa <pktippa@gmail.com>
2018-05-18 13:42:06 +0530

    Added search implementation

--- index.js
+++ index.js
@@ -9,20 +9,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
-
 app.get('/searchBooks', function(req, res) {
   var query = req.query;
-  db.conn(function(err, dbIns) {
-    if (err) return res.send({'error': 'Got some error'});
-    const bookInfoCol = dbIns.collection('BookInfo');
-
-    bookInfoCol.find(query).toArray(function(err, results) {
-      res.send(results);
+  if (query.author || query.genre || query.book_title){ 
+    db.conn(function(err, dbIns) {
+      if (err){ console.log('err', err);return res.send([]);}
+      const bookInfoCol = dbIns.collection('BookInfo');
+  
+      bookInfoCol.find(query).toArray(function(err, results) {
+        res.send(results);
+      });
     });
-  });
+  } else {
+    res.send([]);
+  }  
 });
 
+app.use(express.static('client'));
+
 app.listen(3000);
+if (require.main === module) {
+  console.log('Application is listening on 3000 port, access http://localhost:3000/')
+}
