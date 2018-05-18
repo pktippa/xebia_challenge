4216cee3ff22f34570024f3b9181a92e4cea6039
Pradeep Kumar Tippa <pktippa@gmail.com>
2018-05-18 15:23:54 +0530

    Implemented Login

--- client/js/login_signup.js
+++ client/js/login_signup.js
@@ -1,5 +1,42 @@
 function login(){
-  console.log('Login clicked')
+  console.log('Login clicked');
+  var username = document.getElementById('login_usename').value;
+  var password = document.getElementById('login_password').value;
+  if (username && password) {
+    var obj = {
+      username: username,
+      password: password
+    };
+    // URL want to hit
+    var url = 'http://localhost:3000/login'
+    // Opening up new XMLHttpRequest
+    var http = new XMLHttpRequest()
+    // Making a POST request
+    http.open('POST',url, true)
+    // Setting up Request headers
+    http.setRequestHeader("Content-type","application/json")
+    // Constructing POST data
+    data = obj;
+    // Sending the POST data
+    http.send(JSON.stringify(obj))
+    // On ready state
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        // Checking the readystate is when the HEADERS are recieved
+        if(http.readyState == 4) {
+          if(http.status == 200) {
+            var resJson = JSON.parse(http.responseText);
+            if (resJson.authorized) {
+              sessionStorage.setItem('token', resJson.token);
+            } else {
+              alert('Unauthorized, please login with valid credentials');
+            }            
+          }
+        }
+    }
+  } else {
+    alert('Username and password both are mandatory');
+  }
+  
 }
 
 function signup(){
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const crypto = require('crypto');
+const secret = 'xebia';
+const hmac = crypto.createHmac('sha256', secret)
 var db = require('./db');
+var uuidv4 = require('uuid/v4');
 var app = express();
+var tokens = [];
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +28,40 @@ app.get('/searchBooks', function(req, res) {
   } else {
     res.send([]);
   }  
+});
+
+// Login API POST method
+app.post('/login', function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  var query = {
+    username: username,
+    password: hmac.update(password).digest('hex')
+  };
+  db.conn(function(err, dbIns) {
+    if (err) {console.log('err', err);return res.send([]);}
+    const UsersCol = dbIns.collection('Users');
+    UsersCol.find(query).limit(1).next(function(err, doc){
+      if(err || !doc) {
+        console.log('err',err);
+        return res.send({'authorized': false});
+      }
+      var token = uuidv4();
+      
+      // Caching the tokens
+      // Not implemented any expiry or anything
+      tokens.append({
+        username: doc.username,
+        token: token,
+        role: doc.role
+      });
+      return res.send({
+        'authorized': true,
+        'token': token,
+        role: doc.role 
+      })
+    });
+  })
 });
 
 app.use(express.static('client'));
